bbb683984f06a6042d832b9424252f6fb3a3fcc8
Radhika-2002 <2019radhika.katiyara@ves.ac.in>
2024-06-03 13:25:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function getFormvalue() {
     //Write your code here
-	let first_name = document.getElementById("fname");
-	let last_name= inputs[1].value;
+	let first_name = document.getElementById("fname").value;
+	let last_name = document.getElementById("lname").value;
+	// let last_name= inputs[1].value;
 	let fullName = first_name + " " + last_name;
 	alert(fullName);
 }
